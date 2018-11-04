21c652ef3f872ab9941e234c77307e3c3200d54f
Will Tachau <wtachau@gmail.com>
2018-11-03 22:57:11 -0400

    new folders

--- mysite/mywebsite/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-
-def home(request):
-	context = {}
-	return render(request, 'home.html', {})
