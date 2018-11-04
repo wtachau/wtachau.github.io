32d2ee935a9003d03667f38c4277df1e8f6ebdcd
Will Tachau <wtachau@gmail.com>
2018-11-04 16:03:10 -0500

    cleanup

--- server-transpiled.js
+++ server-transpiled.js
@@ -4,26 +4,29 @@ var _express = _interopRequireDefault(require("express"));
 
 var _path = _interopRequireDefault(require("path"));
 
+var _expressCdn = _interopRequireDefault(require("express-cdn"));
+
 var _webpack = _interopRequireDefault(require("webpack"));
 
 var _webpackDevMiddleware = _interopRequireDefault(require("webpack-dev-middleware"));
 
 var _webpackHotMiddleware = _interopRequireDefault(require("webpack-hot-middleware"));
 
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
 var _webpackDevelopmentConfig = _interopRequireDefault(require("./webpack.development.config.js"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var app = (0, _express.default)();
-var port = 3000;
 var isLocal = !process.env._HANDLER;
 var isDeploying = !!process.env.DEPLOYING;
 var isDevelopment = isLocal && !isDeploying;
-var sslEnabled = !isLocal;
 
 if (isLocal) {
-  require('dotenv').load();
+  _dotenv.default.load();
 }
+
+var app = (0, _express.default)();
 
 if (isDevelopment) {
   var compiler = (0, _webpack.default)(_webpackDevelopmentConfig.default);
@@ -34,7 +37,9 @@ if (isDevelopment) {
   app.use((0, _webpackHotMiddleware.default)(compiler));
 }
 
-var options = {
+app.set('view engine', 'pug');
+app.use(_express.default.static(_path.default.join(__dirname, 'public')));
+app.locals.CDN = (0, _expressCdn.default)(app, {
   publicDir: _path.default.join(__dirname, 'public'),
   viewsDir: _path.default.join(__dirname, 'views'),
   domain: 's3-us-west-1.amazonaws.com/tachauwebsite',
@@ -43,16 +48,10 @@ var options = {
   key: process.env.AWS_ACCESS_KEY_ID,
   secret: process.env.AWS_SECRET_ACCESS_KEY,
   hostname: 'localhost',
-  port: sslEnabled ? 443 : 1337,
-  ssl: sslEnabled,
+  port: isLocal ? 1337 : 443,
+  ssl: !isLocal,
   production: true
-};
-
-var CDN = require('express-cdn')(app, options);
-
-app.set('view engine', 'pug');
-app.use(_express.default.static(_path.default.join(__dirname, 'public')));
-app.locals.CDN = CDN();
+})();
 app.get('/', function (req, res) {
   res.render('home', {
     useBundledAssets: !isDevelopment
@@ -60,7 +59,7 @@ app.get('/', function (req, res) {
 });
 
 if (isLocal && !isDeploying) {
-  app.listen(port);
+  app.listen(3000);
 }
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import path from 'path'
+import expressCDN from 'express-cdn'
 import webpack from 'webpack'
 import webpackMiddleware from 'webpack-dev-middleware'
 import webpackHotMiddleware from 'webpack-hot-middleware'
 import dotenv from 'dotenv'
 
 import webpackConfig from './webpack.development.config.js'
-import configureCDN from './configureCDN'
 
 const isLocal = !process.env._HANDLER
 const isDeploying = !!process.env.DEPLOYING
@@ -31,12 +31,22 @@ if (isDevelopment) {
   app.use(webpackHotMiddleware(compiler))
 }
 
-const CDN = configureCDN(app, isLocal)
-
 app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.locals.CDN = CDN()
+app.locals.CDN = expressCDN(app, {
+  publicDir: path.join(__dirname, 'public'),
+  viewsDir: path.join(__dirname, 'views'),
+  domain: 's3-us-west-1.amazonaws.com/tachauwebsite',
+  bucket: 'tachauwebsite',
+  endpoint: 's3-us-west-1.amazonaws.com',
+  key: process.env.AWS_ACCESS_KEY_ID,
+  secret: process.env.AWS_SECRET_ACCESS_KEY,
+  hostname: 'localhost',
+  port: (isLocal ? 1337 : 443),
+  ssl: !isLocal,
+  production: true
+})()
 
 app.get('/', (req, res) => {
   res.render('home', {
