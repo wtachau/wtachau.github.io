1f02cab984d22dd495770098f440677faab64fee
Will Tachau <wtachau@gmail.com>
2019-08-30 20:32:41 +0900

    all games

--- client/index.js
+++ client/index.js
@@ -39,8 +39,7 @@ const isPaused = () => {
 
 addCanvas();
 
-// const chosenGame = randomElement([tetris, pong, brick, snake, snood])
-const chosenGame = twenty48;
+const chosenGame = randomElement([tetris, pong, brick, snake, snood, twenty48]);
 const args = [animate, defaultRender, isPaused, togglePause];
 
 chosenGame(...args);
